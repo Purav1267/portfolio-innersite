f8c3656d896c7d7b92ed14c0b151a1e035037d55
Purav1267 <puravmalik24@gmail.com>
2025-12-16 22:47:05 +0530

    Update About section content and resume file to Purav_Resume Final OFFCAMPUS 2.pdf

--- src/components/showcase/ResumeDownload.tsx
+++ src/components/showcase/ResumeDownload.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import printer from '../../assets/resume/printer.gif';
-import Resume from '../../assets/resume/Purav_Resume Final OFFCAMPUS.pdf';
+import Resume from '../../assets/resume/Purav_Resume Final OFFCAMPUS 2.pdf';
 
 export interface ResumeDownloadProps {
     altText?: string;
